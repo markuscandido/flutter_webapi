47e94e3339da928b765c49ef57607c7c157140f3
Markus Vinicius Candido <markus.candido@efrete.com.br>
2023-03-28 19:45:21 -0300

    O que aprendemos? Temas no Flutter: Aprendemos que, quando vamos dar continuidade a um projeto que já foi iniciado, muitas vezes precisamos fazer algumas configurações-base para que, ao final, consigamos entregar o que foi pretendido. No nosso caso, a aparência de nossa aplicação estava completamente diferente do pedido em protótipo e por isso a fizemos a alteração utilizando o ThemeData. API e REST: Vimos que, para falar sobre WebAPI, precisamos ter pelo menos uma introdução sobre o que é API e REST. Entendemos que API é uma interface que faz a ligação da comunicação entre cliente e servidor, e que REST é um conjunto de regras que regem a implementação e funcionamento das APIs que são RESTful.

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MVC::Simple Journal',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        textTheme: GoogleFonts.robotoTextTheme(),
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      //themeMode: ThemeMode.system,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
